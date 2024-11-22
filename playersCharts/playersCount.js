3060b73692fc345773446598d88f012dc6e31956
MinMinhMin <xyzhuy2@gmail.com>
2024-11-22 08:45:59 +0700

    playersChartHistoryUpdate

--- playersCharts/playersCount.js
+++ playersCharts/playersCount.js
@@ -16,9 +16,35 @@ async function loadDataset() {
     }));
 }
 
+function unixToDateString(unixTimestamp) {
+    const date = new Date(unixTimestamp * 1000); // Multiply by 1000 to convert seconds to milliseconds
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+
+function analyzing(){
+
+    var list=update_history.update;
+    document.getElementById("update").innerHTML+="All MAJOR updates: <br>";
+    for(let i=0;i<list.length;i++){
+        var date=unixToDateString(list[i][0]),title=list[i][1].replace(/^MAJOR\n/, '');
+        document.getElementById("update").innerHTML+=`${date}: ${title} <br>`;
+    }
+
+
+}
+
 async function initializeChart() {
 
     await loadDataset();
+    analyzing();
 
     document.getElementById("title").textContent="Player count of "+title;
 
@@ -76,5 +102,5 @@ async function initializeChart() {
 }
 
 
-
 initializeChart();
+
