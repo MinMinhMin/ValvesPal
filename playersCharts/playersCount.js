35f59dc7054500eb487890a3efb3df7596afcc22
MinMinhMin <xyzhuy2@gmail.com>
2024-12-04 23:21:20 +0700

    visual fix for playersChart

--- playersCharts/playersCount.js
+++ playersCharts/playersCount.js
@@ -19,7 +19,7 @@ async function loadDataset() {
 function unixToDateString(unixTimestamp) {
     const date = new Date(unixTimestamp * 1000); // convert seconds to milliseconds
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); 
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
@@ -102,7 +102,13 @@ async function initializeChart() {
 
         tooltip: {
             xDateFormat: '%Y-%m-%d %H:%M:%S',
-            shared: true
+            shared: true,
+            formatter: function () {
+                if (this.point) {
+                    return `<b>${Highcharts.dateFormat('%Y-%m-%d', this.x)}</b><br>Số lượng: ${this.y}`;
+                }
+                return 'No data';
+            },
         },
 
         credits: {
